b7db8a95e9336796579608c6ef66c7d99912b177
ntsp <ntsp.gm@gmail.com>
2012-12-24 22:02:16 +1100

    Proper name for server port settings. Version bump.

--- sstyler.py
+++ sstyler.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-# Version: 1.0
+# Version: 1.1
 # See github page for more information:
 # https://github.com/ntsp/SuperStyler
 
 import SuperStyler.menu
 
+
 # Set this to True if you want to manually specify the port to use
-templateserver.use_manual_port = False
-templateserver.port = 9981
+SuperStyler.templateserver.use_manual_port = False
+SuperStyler.templateserver.port = 9981
